c9911492f6fa440b650beec88e3eec2a0268f5de
AkashArjunDalvi <akashdalvi551@gmail.com>
2024-12-22 11:26:37 +0530

    This is the first version

---
+++ test-deploy/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to - Play With Code</h1>
+      <p>This is the first app to deploy on AWS</p>
+    </div>
+  );
+}
+
+export default App;
